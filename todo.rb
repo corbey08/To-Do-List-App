85477cb74e9a571d6796085fa923f58637bd49f8
Greg Corbett <gregcorbett_170@hotmail.com>
2024-12-21 13:50:13 +0000

    Added functionality to delete tasks and added comments

--- todo.rb
+++ todo.rb
@@ -1,7 +1,16 @@
+# Todo List Application
+# Simple CLI app to manage tasks
+
 tasks = []
 
 loop do
-  puts "Choose an option: [1] Add Task [2] View Tasks [3] Mark Task as Done [4] Exit"
+  puts "\n--- Todo List ---"
+  puts "[1] Add Task"
+  puts "[2] View Tasks"
+  puts "[3] Mark Task as Done"
+  puts "[4] Delete Task"
+  puts "[5] Exit"
+  print "Choose an option: "
   choice = gets.chomp.to_i
 
   case choice
@@ -11,13 +20,13 @@ loop do
     tasks << { task: task, completed: false }
     puts "Task added!"
   when 2
-    puts "Your tasks:"
+    puts "\nYour tasks:"
     tasks.each_with_index do |t, i|
       status = t[:completed] ? "[X]" : "[ ]"
       puts "#{i + 1}. #{status} #{t[:task]}"
     end
   when 3
-    puts "Enter the number of the task to mark as done:"
+    print "Enter the number of the task to mark as done: "
     num = gets.chomp.to_i
     if tasks[num - 1]
       tasks[num - 1][:completed] = true
@@ -26,6 +35,15 @@ loop do
       puts "Invalid task number."
     end
   when 4
+    print "Enter the number of the task to delete: "
+    num = gets.chomp.to_i
+    if tasks[num - 1]
+      tasks.delete_at(num - 1)
+      puts "Task deleted!"
+    else
+      puts "Invalid task number."
+    end
+  when 5
     puts "Goodbye!"
     break
   else
