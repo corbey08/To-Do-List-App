0a8e68cec7a42e10ac5b9abde52653b3214f800f
Greg Corbett <gregcorbett_170@hotmail.com>
2024-12-21 13:46:10 +0000

    Added functionality to add and view tasks

--- todo.rb
+++ todo.rb
@@ -1,1 +1,22 @@
-puts "Welcome to To Do List"
+tasks = []
+
+loop do
+  puts "Choose an option: [1] Add Task [2] View Tasks [3] Exit"
+  choice = gets.chomp.to_i
+
+  case choice
+  when 1
+    print "Enter task: "
+    task = gets.chomp
+    tasks << task
+    puts "Task added!"
+  when 2
+    puts "Your tasks:"
+    tasks.each_with_index { |t, i| puts "#{i + 1}. #{t}" }
+  when 3
+    puts "Goodbye!"
+    break
+  else
+    puts "Invalid option. Try again."
+  end
+end
